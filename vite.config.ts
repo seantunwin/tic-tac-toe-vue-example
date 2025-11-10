06dd07bef9eda7f381f10b359bed4778dc1c8b81
Sean T. Unwin <2191418+seantunwin@users.noreply.github.com>
2025-11-10 17:22:51 -0500

    build(github-pages): initialize github-pages (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/tic-tac-toe-vue-example/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
